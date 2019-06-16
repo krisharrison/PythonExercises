e9a30d116e16cdd7b0ba8804911324c63a19da73
KrisHarrison <KrisHarrison143@gmail.com>
2019-06-16 02:15:20 -0400

    Python Exercises

---
+++ Ecommerce/Shipping.py
@@ -0,0 +1,5 @@
+def calc_shipping():
+    print("calc_shipping")
+
+def calc_tax():
+    print("calc_tax")
---
+++ Gambling/Dice.py
@@ -0,0 +1,8 @@
+import random
+
+def roll():
+
+    first_number = random.randint(1,6)
+    second_number = random.randint(1, 6)
+
+    return first_number, second_number
